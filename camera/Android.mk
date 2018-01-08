ee0bbf981e205c3347fd4f3267cba8507bbd035a
Eric Clothier <eric.clothier@gmail.com>
2018-01-08 14:34:25 -0500

    hlte-common: Update library dependencies for camera wrapper
    Change-Id: I347571508367eede24e8fb2092e2732423b63ecf

--- camera/Android.mk
+++ camera/Android.mk
@@ -7,7 +7,15 @@ LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils
+    libhardware \
+    liblog \
+    libcamera_client \
+    libgui \
+    libhidltransport \
+    libsensor \
+    libutils \
+    android.hidl.token@1.0-utils \
+    android.hardware.graphics.bufferqueue@1.0
 
 LOCAL_C_INCLUDES := \
     system/media/camera/include
@@ -15,6 +23,8 @@ LOCAL_C_INCLUDES := \
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.msm8974
 LOCAL_MODULE_TAGS := optional
+LOCAL_STATIC_LIBRARIES := libbase libarect android.hidl.token@1.0
+LOCAL_HEADER_LIBRARIES := libnativebase_headers
 
 include $(BUILD_SHARED_LIBRARY)
 
