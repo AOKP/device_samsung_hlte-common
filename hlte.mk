8f42c5b86a8e88be74ef4d3f884d6de4f0454ebd
Kevin F. Haggerty <haggertk@lineageos.org>
2018-01-17 21:08:19 -0700

    hlte-common: Drop GPS configs/PRODUCT_PACKAGES/blobs
    * These have moved to msm8974-common
    
    Change-Id: Ib4cd06549a862f098da692b3a9bd111fd9e9eab7

--- hlte.mk
+++ hlte.mk
@@ -67,15 +67,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     FlipFlap
 
-# GPS
+# Gestures
 PRODUCT_PACKAGES += \
-    gps.msm8974
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/configs/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/configs/sap.conf:system/etc/sap.conf
+    com.cyanogenmod.keyhandler
 
 # camera
 PRODUCT_PACKAGES += \
