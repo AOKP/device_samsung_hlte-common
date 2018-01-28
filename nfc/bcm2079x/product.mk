LineageOS/android_device_samsung_hlte-common

hlte-common: Fragment NFC support based on chip type
* Configs and board/product makefiles are split into discrete
  directories based on NFC chip. These chain to truly common
  makefiles that add permissions and shared PRODUCT_PACKAGES.
* Relocate libpn547_fw under the nfc/pn547 directory.

Change-Id: Ie9e87fc0566eac76fc411b3e5669564c5fa54123

nfc/bcm2079x/product.mk
@@ -1,0 +1,28 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# NFC
+$(call inherit-product, device/samsung/klte-common/nfc/product.mk)
+
+PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.0-impl-bcm \
+    android.hardware.nfc@1.0-service \
+    nfc_nci.bcm2079x.default
+
+PRODUCT_COPY_FILES += \
+    device/samsung/klte-common/nfc/bcm2079x/libnfc-brcm.conf:system/vendor/etc/libnfc-brcm.conf \
+    device/samsung/klte-common/nfc/bcm2079x/libnfc-brcm-20791b04.conf:system/vendor/etc/libnfc-brcm-20791b04.conf \
+    device/samsung/klte-common/nfc/bcm2079x/libnfc-brcm-20791b05.conf:system/vendor/etc/libnfc-brcm-20791b05.conf

nfc/pn547/product.mk
@@ -1,5 +1,5 @@
-# Copyright 2016 The CyanogenMod Project
-# Copyright 2017-2018 The LineageOS Project
+#
+# Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,4 +14,14 @@
 # limitations under the License.
 #
 
-include $(call first-makefiles-under,$(call my-dir))
+# NFC
+$(call inherit-product, device/samsung/klte-common/nfc/product.mk)
+
+PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.0-impl \
+    libpn547_fw \
+    nfc_nci.msm8974
+
+PRODUCT_COPY_FILES += \
+    device/samsung/klte-common/nfc/pn547/libnfc-brcm.conf:system/vendor/etc/libnfc-brcm.conf \
+    device/samsung/klte-common/nfc/pn547/libnfc-nxp.conf:system/vendor/etc/libnfc-nxp.conf

AOKP/device_samsung_hlte-common

nfc/bcm2079x/product.mk
@@ -1,0 +1,28 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# NFC
+$(call inherit-product, device/samsung/hlte-common/nfc/product.mk)
+
+PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.0-impl-bcm \
+    android.hardware.nfc@1.0-service \
+    nfc_nci.bcm2079x.default
+
+PRODUCT_COPY_FILES += \
+    device/samsung/hlte-common/nfc/bcm2079x/libnfc-brcm.conf:system/vendor/etc/libnfc-brcm.conf \
+    device/samsung/hlte-common/nfc/bcm2079x/libnfc-brcm-20791b04.conf:system/vendor/etc/libnfc-brcm-20791b04.conf \
+    device/samsung/hlte-common/nfc/bcm2079x/libnfc-brcm-20791b05.conf:system/vendor/etc/libnfc-brcm-20791b05.conf

nfc/pn547/product.mk
@@ -1,5 +1,5 @@
-# Copyright 2016 The CyanogenMod Project
-# Copyright 2017 The LineageOS Project
+#
+# Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,4 +14,14 @@
 # limitations under the License.
 #
 
-include $(call first-makefiles-under,$(call my-dir))
+# NFC
+$(call inherit-product, device/samsung/hlte-common/nfc/product.mk)
+
+PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.0-impl \
+    libpn547_fw \
+    nfc_nci.msm8974
+
+PRODUCT_COPY_FILES += \
+    device/samsung/hlte-common/nfc/pn547/libnfc-brcm.conf:system/vendor/etc/libnfc-brcm.conf \
+    device/samsung/hlte-common/nfc/pn547/libnfc-nxp.conf:system/vendor/etc/libnfc-nxp.conf